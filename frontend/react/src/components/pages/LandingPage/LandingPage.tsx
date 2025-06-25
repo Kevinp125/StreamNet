63ff1e1542472be0cae6726bf3ecacdc11416d07
Kevin Pereda <kevinpereda125@meta.com>
2025-06-25 16:45:16 -0700

    Added streamnet logo

--- frontend/react/src/components/pages/LandingPage/LandingPage.tsx
+++ frontend/react/src/components/pages/LandingPage/LandingPage.tsx
@@ -8,13 +8,13 @@ export default function LandingPage() {
   return (
     <section className = "landing-section">
       <div className = "left-info">
+        <img src="/Streamnet.png" alt="" />
         <h1>StreamNet</h1>
         <p>Connecting streamers <br/> one collab at a time</p>
       </div>
 
       <aside className = "login-aside">
-
-      <ContentContainer></ContentContainer>
+        <h2>Login / Signup </h2>
       </aside>
 
     </section>
