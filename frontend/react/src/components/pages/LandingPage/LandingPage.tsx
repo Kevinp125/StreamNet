c7fe6d65bfec4d45a5297549cd2d3e28775a8a10
Kevin Pereda <kevinpereda125@meta.com>
2025-06-25 16:24:01 -0700

    getting cool techy font

--- frontend/react/src/components/pages/LandingPage/LandingPage.tsx
+++ frontend/react/src/components/pages/LandingPage/LandingPage.tsx
@@ -1,19 +1,14 @@
 import styled from "styled-components";
 import ContentContainer from "../../ContentContainer/ContentContainer";
 import './landingpage.css'
-const Header = styled.h1`
-  color: white;
-`;
+
+
 
 export default function LandingPage() {
   return (
     <section className = "landing-section">
       <div className = "left-info">
-        <Header>Landing Page</Header>
-      </div>
-
-      <div className = "line-separator">
-
+        <h1>StreamNet</h1>
       </div>
 
       <aside className = "login-aside">
