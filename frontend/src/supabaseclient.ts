ace6eeea1583f863241a1b8555187f6e9b3dc724
Kevin Pereda <kevinpereda125@meta.com>
2025-06-26 13:39:14 -0700

    added file which creates a supabase client that will allow us to use its functions

---
+++ frontend/src/supabaseclient.ts
@@ -0,0 +1,8 @@
+//this file legit just makes a supabase client for us
+
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL, // Supabase URL
+  import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY // Supabase anon key
+);
