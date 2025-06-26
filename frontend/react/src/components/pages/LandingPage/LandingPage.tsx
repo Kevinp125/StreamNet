02303b5da089f032a06665f694c31ed4d22181cf
Kevin Pereda <kevinpereda125@meta.com>
2025-06-26 10:31:35 -0700

    Added prettier plugin that helps organize and format my tailwind styles to be grouped together based on util

--- frontend/react/src/components/pages/LandingPage/LandingPage.tsx
+++ frontend/react/src/components/pages/LandingPage/LandingPage.tsx
@@ -4,15 +4,16 @@ import styled from "styled-components";
 export default function LandingPage() {
   return (
     <section className="flex h-screen items-center">
-      <div className="flex-2 flex flex-col justify-center items-center">
-        <img src="/Streamnet.png" alt="" className="h-96 w-[500px]" /> {/* width still arbitrary */}
-        <h1 className="text-6xl text-white text-center">StreamNet</h1>
-        <p className="text-2xl text-white text-center">
+      <div className="flex flex-2 flex-col items-center justify-center">
+        <img src="/Streamnet.png" alt="" className="h-96 w-[500px]" />{" "}
+        {/* width still arbitrary */}
+        <h1 className="text-center text-6xl text-white">StreamNet</h1>
+        <p className="text-center text-2xl text-white">
           Connecting streamers <br /> one collab at a time
         </p>
       </div>
 
-      <aside className="flex-1 h-full bg-light-purple flex flex-col justify-center items-center">
+      <aside className="bg-light-purple flex h-full flex-1 flex-col items-center justify-center">
         <h2 className="text-2xl text-white">Login / Signup</h2>
       </aside>
     </section>
