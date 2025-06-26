656ded76f23bdf335bd8ee6dee2d84f042d747ab
Kevin Pereda <kevinpereda125@meta.com>
2025-06-26 08:37:18 -0700

    Getting tailwindcss configured. Going to use that for rest of prohect instead

--- frontend/react/src/components/pages/LandingPage/LandingPage.tsx
+++ frontend/react/src/components/pages/LandingPage/LandingPage.tsx
@@ -1,22 +1,20 @@
 import styled from "styled-components";
-import './landingpage.css'
-
-
+// import "./landingpage.css";
 
 export default function LandingPage() {
   return (
-    <section className = "landing-section">
-      <div className = "left-info">
-        <img src="/Streamnet.png" alt="" />
-        <h1>StreamNet</h1>
-        <p>Connecting streamers <br/> one collab at a time</p>
+    <section className="flex h-screen items-center">
+      <div className="flex-2 flex flex-col justify-center items-center">
+        <img src="/Streamnet.png" alt="" className="h-96 w-[500px]" /> {/* width still arbitrary */}
+        <h1 className="text-6xl text-white text-center">StreamNet</h1>
+        <p className="text-2xl text-white text-center">
+          Connecting streamers <br /> one collab at a time
+        </p>
       </div>
 
-      <aside className = "login-aside">
-        <h2>Login / Signup </h2>
-
+      <aside className="flex-1 h-full bg-purple-300 flex flex-col justify-center items-center">
+        <h2 className="text-2xl text-white">Login / Signup</h2>
       </aside>
-
     </section>
   );
 }
