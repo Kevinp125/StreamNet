ab33ba68d524f6b7e46f96b2271c2d41937474b2
Kevin Pereda <kevinpereda125@meta.com>
2025-06-27 14:00:54 -0700

    Got a temporary Discover Streamers button on the Dash page that when clicked goes to discover page where user can view navbar and use it to navigate between other pages

--- frontend/src/components/pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/components/pages/DashboardPage/DashboardPage.tsx
@@ -1,3 +1,16 @@
+import { Button } from "../../ui/button";
+import { useNavigate } from "react-router-dom";
+
 export default function DashboardPage() {
-  return <p className="text-white">In Dash</p>;
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1 className="text-white">In Dash</h1>
+      <Button onClick = {() => {navigate('/discover')}} className="position bg-electric-indigo hover: absolute right-20 bottom-20 h-16 w-72 cursor-pointer transition-transform duration-200 ease-in-out hover:scale-105">
+        Discover Streamers!
+      </Button>
+    </>
+  );
 }
