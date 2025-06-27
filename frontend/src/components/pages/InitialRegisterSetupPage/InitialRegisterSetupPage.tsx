a01d33bc5df36125fc396473ce21d7185d3c7b14
Kevin Pereda <kevinpereda125@meta.com>
2025-06-27 11:27:22 -0700

    Had an await outside of an async function

--- frontend/src/components/pages/InitialRegisterSetupPage/InitialRegisterSetupPage.tsx
+++ frontend/src/components/pages/InitialRegisterSetupPage/InitialRegisterSetupPage.tsx
@@ -1,9 +1,6 @@
 import { supabase } from "@/supabaseclient";
 import { useEffect } from "react";
 
-const sessionResult = await supabase.auth.getSession();
-const token = sessionResult.data?.session?.access_token;
-
 export default function InitialRegisterSetupPage() {
   {
     /*In here is where fetch requesto backend to setup profile is going to take plac. You will send in Authorization header the token we get from session above  */
@@ -12,6 +9,14 @@ export default function InitialRegisterSetupPage() {
   useEffect(() => {
     async function checkUserProfile() {
       try {
+        const sessionResult = await supabase.auth.getSession();
+        const token = sessionResult.data?.session?.access_token;
+
+        if (!token) {
+          console.error("No token grabbed from session");
+          return;
+        }
+
         const res = await fetch(
           `${import.meta.env.VITE_SERVER_URL}/api/auth/check-user`,
           {
