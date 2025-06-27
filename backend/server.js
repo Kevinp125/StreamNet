c9b29e14e14dd0eb4d060e83f79617da89c12c50
Kevin Pereda <kevinpereda125@meta.com>
2025-06-27 11:56:44 -0700

    Allowing cors to accept all types of cross origin sharing not just local host instances

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,12 @@ const cors = require("cors"); //allows for cross origin sharing. Since frontend 
 const routes = require("./api/routes/index");
 
 const app = express(); //creating an instance of the express application
-app.use(cors()); //allows app to use cross origin sharing
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json()); //tells Express to automatically parse incoming requests with Content-Type: application/json and put the parsed data on req.body
 
 app.use("/api", routes); // all routes mounted here so they auto have /api in front of them which is important because that is what vercel needs to handle requests
@@ -14,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   return res.status(200).send("hello");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
