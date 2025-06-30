c45996ef9b0719ed2abb2bbfe0c7520927732389
Kevin Pereda <kevinpereda125@meta.com>
2025-06-30 10:45:23 -0700

    Added a hook in the AuthProvider file that just uses the useContext function.

--- frontend/src/Context/AuthProvider.tsx
+++ frontend/src/Context/AuthProvider.tsx
@@ -1,6 +1,6 @@
-import type { Session, User} from '@supabase/supabase-js';
-import { useContext, useState, useEffect, createContext } from 'react';
-import { supabase } from '@/supabaseclient';
+import type { Session, User } from "@supabase/supabase-js";
+import { useContext, useState, useEffect, createContext } from "react";
+import { supabase } from "@/supabaseclient";
 
 //context type
 type AuthContextType = {
@@ -8,41 +8,47 @@ type AuthContextType = {
   user: User | null;
   loading: boolean;
   signOut: () => void;
-}
+};
 //creating the context with default values for the session, user, and signOut method
-const AuthContext = createContext<AuthContextType> ({session: null, user: null, loading:true, signOut: () =>{}})
+const AuthContext = createContext<AuthContextType>({
+  session: null,
+  user: null,
+  loading: true,
+  signOut: () => {},
+});
 
-export function AuthProvider({children}: any){
-  const [user, setUser] = useState<User | null>(null)
+export function AuthProvider({ children }: any) {
+  const [user, setUser] = useState<User | null>(null);
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true); //true by default cause its loading until info is loaded
 
-
   //on page mount we want to check the users auth status
-  useEffect(() =>{
+  useEffect(() => {
     //function to populate all the data that will be stored in the context. session user details etc
-    async function setData(){
-      const { data: { session }, error } = await supabase.auth.getSession();
+    async function setData() {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
       if (error) throw error;
-      setSession(session)
-      setUser(session?.user || null)
+      setSession(session);
+      setUser(session?.user || null);
       setLoading(false); //dont forget to set loading to false afterwards
-    };   
+    }
 
     //this onAuthStateChange is a supabase provided function that listens to whenever user signs in, signs out, token refreshes, etc. As soon as any of this happens event is fired and all context state is updated accordingly
     const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
-      setUser(session?.user || null)
-      setLoading(false)
+      setUser(session?.user || null);
+      setLoading(false);
     });
 
     setData(); //call function that sets the user Data
 
     //this runs on component unmount so when app closes we unsubscribe from supabase AuthStateChange listener to prevent leaks
     return () => {
-        listener?.subscription.unsubscribe();
+      listener?.subscription.unsubscribe();
     };
-
   }, []);
 
   //now that useEffect executed stores all the state values in value which will be passed to provider
@@ -50,13 +56,15 @@ export function AuthProvider({children}: any){
     session,
     user,
     loading,
-    signOut: () => supabase.auth.signOut(), //supabase signout function just store in our signOut for better readability. Other components can acess everywhere
+    signOut: async () => {
+      await supabase.auth.signOut();
+    }, //supabase signout function just store in our signOut for better readability. Other components can acess everywhere
   };
 
-  return(
-    <AuthContext.Provider value = {value}>
-      {children}
-    </AuthContext.Provider>
-  )
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>; //return the context as a provider wrapper with the given values 
+}
 
+//this a hook we are exporting that can be called in any component just makes our code cleaner. useContext returns an object with all the values set in context
+export function useAuthContext(){
+  return useContext(AuthContext);
 }
