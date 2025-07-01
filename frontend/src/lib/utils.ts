9082da1481e69bc89d822af3f8012eb805825d3b
Kevin Pereda <kevinpereda125@meta.com>
2025-07-01 10:41:45 -0700

    Got the first function populate streamerCard coded. This function is within the dashboard page and gets called whenever the session gets mounted / changes. It calls out fetchStreamerCardInfo function which returns the profile from the api call and sets a profile state so we can pass it to the streamer card component. Also got all the typing for the prop for streamer card component settled.

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,30 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const API_BASE_URL = import.meta.env.VITE_SERVER_URL
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function fetchStreamerCardInfo(accessToken: string){
+  try{
+    
+
+
+
+
+
+  } catch(err){
+
+
+
+
+
+
+
+
+  }
+
+
 }
