ed9005cac2ffa824b08bb71c0bac2b102919998c
Kevin Pereda <kevinpereda125@meta.com>
2025-06-30 20:49:17 -0700

    just making the flex boxes on the dashboard that will contain all the components such as the streamer card, prompt of day, and recent activity

--- frontend/src/pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage/DashboardPage.tsx
@@ -7,16 +7,32 @@ export default function DashboardPage() {
 
   //keep in mind in future below discover streamers button wont be positioned absolutely but rather be in a flex box container that also contains prompt of day and recent activity
   return (
-    <>
-      <h1 className="text-white">In Dash</h1>
-      <Button
-        onClick={() => {
-          navigate(DISCOVER_PATH);
-        }}
-        className="position bg-electric-indigo absolute right-20 bottom-20 h-16 w-72 cursor-pointer transition-transform duration-200 ease-in-out hover:scale-105"
-      >
-        Discover Streamers!
-      </Button>
-    </>
+    
+    <div className = "flex flex-row gap-6 p-6">
+
+      <div className = "flex-1">
+        {/*Streamer card component goes here */}
+      </div>
+
+      <div className = "flex flex-col gap-6 flex-1 items-center">
+
+        {/*Prompt of day and recent avtiviy components go here */}
+      
+      
+        <Button
+          onClick={() => {
+            navigate(DISCOVER_PATH);
+          }}
+          className="position bg-electric-indigo h-16 w-72 cursor-pointer transition-transform duration-200 ease-in-out hover:scale-105"
+        >
+          Discover Streamers!
+        </Button>
+
+      </div>
+
+
+    </div>
+
+    
   );
 }
