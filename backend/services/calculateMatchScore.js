31dd8d75a18f108e83d64344b9fa44c33a0b422c
Kevin Pereda <kevinpereda125@meta.com>
2025-07-02 13:37:11 -0700

    Adjusted how I am giving points based off age. Still awarding points if within a certain gap of 10 years. However, the bigger the age gap the less points. Also added check if users stream to same target audience

--- backend/services/calculateMatchScore.js
+++ backend/services/calculateMatchScore.js
@@ -1,26 +1,35 @@
-const { differenceInYears} = require('date-fns');
+const { differenceInYears } = require("date-fns");
 
 //function returns the match score between the currentUser and a streamer based off values. The higher the score the better the match
-function calculateMatchScore(currentUser, streamerToCompare){
+function calculateMatchScore(currentUser, streamerToCompare) {
   //to start off score can only go up.
   const score = 0;
 
-  //converting the date of births of both the current user and streamer we are comparing with to years so we can check if they are close in age. 
-  const currentUserAge = differenceInYears(new Date(), new Date(currentUser.date_of_birth));
-  const streamerAge = differenceInYears(new Date(), new Date(streamerToCompare.date_of_birth));
+  //converting the date of births of both the current user and streamer we are comparing with to years so we can check if they are close in age.
+  const currentUserAge = differenceInYears(
+    new Date(),
+    new Date(currentUser.date_of_birth)
+  );
+  const streamerAge = differenceInYears(
+    new Date(),
+    new Date(streamerToCompare.date_of_birth)
+  );
 
-  //if the user and streamer we are comparing them to are withing three years of age add a point to the score
-  if(Math.abs(currentUserAge - streamerAge) <= 3){
+  //get the age difference and depending on how far the gap is we assign more points if the gap is closer and less if further apart
+  const ageDifference = Math.abs(currentUserAge - streamerAge);
+
+  if (ageDifference <= 2) {
+    score += 4;
+  } else if (ageDifference <= 5) {
+    score += 2;
+  } else if (ageDifference <= 10) {
     score++;
   }
 
-
-
-
-
+  //check targetAudience. This one is a BIG boost. If user and streamer we are comparing them with stream to same audience give them 3 points
+  if (currentUser.targetAudience === streamerToCompare.targetAudience) {
+    score = score + 6;
+  }
 
   return score;
-
-
-
 }
