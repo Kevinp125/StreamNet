fbc224dce35b5805b461cea81d141e796b011742
Kevin Pereda <kevinpereda125@meta.com>
2025-07-02 11:24:14 -0700

    Added a new Api that fetches all the streamers from the database in most recently added order. Also included a query that snags all streamers except the current user thats logged in. Tested it on insomnia and it is working well so that is good to go

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -24,4 +24,29 @@ router.route("/me").get(authenticateMiddleware, async (req, res) => {
   }
 });
 
+router.route("/get-all").get(authenticateMiddleware, async (req, res) => {
+  try {
+    //putting client that we attached to request in a variable for ease of use
+    const supabase = req.supabase;
+
+    //fetch all profile rows
+    const { data: streamers, error } = await supabase
+      .from("profiles")
+      .select("*")
+      //neq means select all profiles that arent equal to current user session id. We dont want oursevles showing up in recommendation
+      .neq("id", req.user.id)
+      //ascending false allows us to get newly created streamers first
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      throw error;
+    }
+
+    return res.status(200).json(streamers);
+  } catch (err) {
+    console.error("Error fetching all the streamers", err);
+    return res.status(500).json({ error: "Failed to fetch streamers" });
+  }
+});
+
 module.exports = router;
