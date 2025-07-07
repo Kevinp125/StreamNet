519b98dd88b32086c55f4049c600f6322abd7dc2
Kevin Pereda <kevinpereda125@meta.com>
2025-07-07 15:27:29 -0700

    for post connections route already extracting the information. Getting the streamer id we connected with from what was passed in the body. GEtting user id from middle ware as well as supabase client. Then inserting connection in database.

--- backend/api/routes/connections.js
+++ backend/api/routes/connections.js
@@ -5,7 +5,23 @@ const router = express.Router(); //making a router
 
 //Route will add a connection to the database table 
 router.route("/add").post(authenticateMiddleware, async (req, res) => {
+  try{
+    const { connected_streamer_id} = req.body;
+    const user_id = req.user.id;
+    const supabaseClient = req.supabase
 
+    const { error } = await supabaseClient.from('connections').insert({ 
+        user_id: user_id, 
+        connected_streamer_id: connected_streamer_id 
+      });
+
+
+
+
+
+
+
+  }
 
 
 })
