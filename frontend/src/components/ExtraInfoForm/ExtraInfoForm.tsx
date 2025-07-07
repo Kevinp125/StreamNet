61595810288c34afc0c4465baa15d12a2376423c
Kevin Pereda <kevinpereda125@meta.com>
2025-07-07 10:40:28 -0700

    This series of commits is purely just revisions and adjustments I am making to improve code based on manuel's feedback. Here adde a map for the select content dropdown to make code cleaner / more modular

--- frontend/src/components/ExtraInfoForm/ExtraInfoForm.tsx
+++ frontend/src/components/ExtraInfoForm/ExtraInfoForm.tsx
@@ -24,6 +24,16 @@ type ExtraInfoFormProps = {
 };
 
 export default function ExtraInfoForm({ handleFormSubmit }: ExtraInfoFormProps) {
+
+  const AUDIENCES = [
+    {id: 'kids', label: 'Kids (0-12)'},
+    {id: 'teens', label: 'Teens (13-17)'},
+    {id: 'young_adults', label: 'Young Adults (18-25)'},
+    {id: 'adults', label: 'Adults (26-35)'},
+    {id: 'mature', label: 'Mature (35+)'},
+    {id: 'all_ages', label: 'All Ages'},
+  ];
+
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
 
@@ -78,12 +88,9 @@ export default function ExtraInfoForm({ handleFormSubmit }: ExtraInfoFormProps) 
                   <SelectValue placeholder='Select your target audience' />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value='kids'>Kids (0-12)</SelectItem>
-                  <SelectItem value='teens'>Teens (13-17)</SelectItem>
-                  <SelectItem value='young_adults'>Young Adults (18-25)</SelectItem>
-                  <SelectItem value='adults'>Adults (26-35)</SelectItem>
-                  <SelectItem value='mature'>Mature (35+)</SelectItem>
-                  <SelectItem value='all_ages'>All Ages</SelectItem>
+                  {AUDIENCES.map(audience => (
+                    <SelectItem id={audience.id} value={audience.id}>{audience.label}</SelectItem>
+                  ))}
                 </SelectContent>
               </Select>
             </div>
