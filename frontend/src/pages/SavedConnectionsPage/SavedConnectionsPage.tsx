1790a61f7510ed1ed0497bad28ad830f712af3b9
Kevin Pereda <kevinpereda125@meta.com>
2025-07-08 13:06:53 -0700

    Importing everything I need in order to populate the saved connections page

--- frontend/src/pages/SavedConnectionsPage/SavedConnectionsPage.tsx
+++ frontend/src/pages/SavedConnectionsPage/SavedConnectionsPage.tsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from "react";
+import StreamerGrid from "@/components/StreamerGrid/StreamerGrid";
+import StreamerCard from "@/components/StreamerCard/StreamerCard";
+import { getUserConnections } from "@/lib/api_client";
+import { useAuthContext } from "@/Context/AuthProvider";
+import type { StreamerProfile } from "@/types/AppTypes";
+
+
 export default function SavedConnectionsPage(){
   return(
     <p className = "text-white">In Saved Connections</p>
