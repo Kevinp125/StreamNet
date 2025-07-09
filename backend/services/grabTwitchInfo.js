99fc236541c3471523af3b862d5eeff67d1643ac
Kevin Pereda <kevinpereda125@meta.com>
2025-07-09 10:45:17 -0700

    Finished writing function that gets the access token we need in order to make fetch requests that get public user data

--- backend/services/grabTwitchInfo.js
+++ backend/services/grabTwitchInfo.js
@@ -1,16 +1,24 @@
 //below function fetches an access token given by twitch through our client secret and id. This token will allow us to successfully make fetch request to get user channel data
-async function getTwitchAppAccessToken(){
+async function getTwitchAppAccessToken() {
+  try {
+    const res = await fetch("https://id.twitch.tv/oauth2/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: `client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`,
+    });
 
+    if (!res.ok) {
+      throw new Error(`Could not get the twitch app token:${res.status}`);
+    }
 
-
-
+    const token = await response.json();
+    //above returns a token object with like 3 different fields we just want access token field.
+    return token.access_token;
+  } catch (err) {
+    console.error("Could not get the twitch app token", err);
+    throw err;
+  }
 }
-
-
 
 //this function will make fetch request to twitch api with access token we got and retrieve some extra channel information such as the language, game last played, and twitch's own tags
-async function getTwitchChannelData(){
-  
-
-}
-  
+async function getTwitchChannelData() {}
