3c021af30622847e559d4365fe450f25fb53992f
Kevin Pereda <kevinpereda125@meta.com>
2025-07-09 15:03:34 -0700

    also got the userWeights corresponding to user so we can update them accordingly. Checking through my calcScore functions if the streamer user clicked on matches his stuff. depending on that weights will be adjusted

--- backend/api/routes/userActions.js
+++ backend/api/routes/userActions.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { authenticateMiddleware } = require("../../middleware/authRequest");
 const router = express.Router(); //making a router
+const { calcAgeScore, calcAudienceScore, calcLanguageScore, calcGameScore } = require("../../services/calculateMatchScore.js");
 
 //api route fires whenever a user connects with a streamer.
 //Instead of saving a connection or anything though this route handles updating the user weights table depending on what attributes match the user based off that streamer
@@ -21,6 +22,29 @@ router.route("/connect").post(authenticateMiddleware, async (req, res) => {
       .select("*")
       .eq("id", streamerId)
       .single();
+
+    //get what the users weights are right now
+    const { data: userWeights } = await req.supabase
+      .from("user_weights")
+      .select("*")
+      .eq("user_id", userId)
+      .single();
+
+    //remember calcAgeScore returns 2 or 4 if ages are closer so if its greater than 1 we consider it that user cares about age
+    const ageClose = calcAgeScore(user,streamer) > 1;
+    //same thing language matches if calc language checks them and they return greater than 1 cause they are same
+    const languageMatch = calcLanguageScore(user, streamer) > 1;
+    const gameMatch = calcGameScore(user,streamer) >1;
+    const audienceMatch = calcAudienceScore(user,streamer) >1;
+
+    //now after we checked why user connected with this streamer. Did he give importance to languages matching, games matching, audiences matching?
+    //we update all the weights based on the matching
+    if (ageClose) userWeights.age_weight += 0.1;
+    if (audienceMatch) userWeights.audience_weight += 0.1;
+    if (gameMatch) userWeights.game_weight += 0.1;
+    if (languageMatch) userWeights.language_weight += 0.1;
+
+
   } catch (err) {}
 });
 
