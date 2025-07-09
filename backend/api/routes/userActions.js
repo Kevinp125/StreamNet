4e301ace005ee79bd38ebd733db6dfb4e8b02be7
Kevin Pereda <kevinpereda125@meta.com>
2025-07-09 14:40:04 -0700

    Working on route that will update the user weights table after they click connect depending on similarties to streamer they connected with. First getting the user and streamer using supabase so we can compare

--- backend/api/routes/userActions.js
+++ backend/api/routes/userActions.js
@@ -2,13 +2,26 @@ const express = require("express");
 const { authenticateMiddleware } = require("../../middleware/authRequest");
 const router = express.Router(); //making a router
 
-router.route("/connect").post( authenticateMiddleware, async (req, res) => {
+//api route fires whenever a user connects with a streamer.
+//Instead of saving a connection or anything though this route handles updating the user weights table depending on what attributes match the user based off that streamer
+router.route("/connect").post(authenticateMiddleware, async (req, res) => {
+  try {
+    const { streamerId } = req.body;
+    const userId = req.user.id;
 
+    //get data of user that is logged in as well as streamer that was clicked on which will be passed through the body
+    const { data: user } = await req.supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", userId)
+      .single();
 
-
-
-
-})
-
+    const { data: streamer } = await req.supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", streamerId)
+      .single();
+  } catch (err) {}
+});
 
 module.exports = router;
