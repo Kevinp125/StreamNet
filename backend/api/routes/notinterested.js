fc8225dd6a6c5d2381c86ec2e52bee749795ca6b
Kevin Pereda <kevinpereda125@meta.com>
2025-07-10 15:10:10 -0700

    First thing we are doing in api is just adding the streamer that was clicked on into the not_interested table so that when we fetch streamers for a user we can exclude the ones they have labeled as not interested

--- backend/api/routes/notinterested.js
+++ backend/api/routes/notinterested.js
@@ -2,6 +2,19 @@ const express = require("express");
 const { authenticateMiddleware } = require("../../middleware/authRequest");
 const router = express.Router(); //making a router
 
+//learning from past so this route is going to add a streamer to the not interested table as well as updating the weights for our algorithm negatively since user wasnt interested
+router.route("/").post(authenticateMiddleware, async (req, res) => {
+  const { streamerId } = req.body;
+  const userId = req.user.id;
 
+  const { error: notInterestedError } = await req.supabase
+    .from("user_not_interested")
+    .insert({
+      user_id: userId,
+      streamer_id: streamerId,
+    });
+
+  if (notInterestedError) throw notInterestedError;
+});
 
 module.exports = router;
