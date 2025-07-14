fb3dd425498421efd5da17e385e788fc834a7d14
Kevin Pereda <kevinpereda125@meta.com>
2025-07-14 12:55:20 -0700

    added file that will be where we put the code for our create event form component. Also added state to determine when to have form appearing vs not appeariing

--- frontend/src/pages/EventsPage/EventsPage.tsx
+++ frontend/src/pages/EventsPage/EventsPage.tsx
@@ -1,15 +1,19 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
-
-export default function EventsPage(){
-  return(
-    <div className = "flex flex-col items-center p-16" >
-      <Button className = "w-72 h-16 bg-electric-indigo">
+export default function EventsPage() {
+  const [showEventForm, setShowEventForm] = useState(false);
+  return (
+    <div className='flex flex-col items-center p-16'>
+      <Button className='bg-electric-indigo h-16 w-72' onClick={() => setShowEventForm(true)}>
         Post Event!
       </Button>
 
+      {/*TODO here we will have the event grid that will map though all events and display them*/}
 
-
+      {showEventForm && (
+        <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4'></div>
+      )}
     </div>
-  )
+  );
 }
