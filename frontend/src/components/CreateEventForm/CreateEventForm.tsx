b9d372abebc7100d61809dfc8deef0feaa023847
Kevin Pereda <kevinpereda125@meta.com>
2025-07-14 14:22:30 -0700

    Starting to get the post event form going. Grabbed from the extra info form and jsut changing values to fit what would go on the create event form

--- frontend/src/components/CreateEventForm/CreateEventForm.tsx
+++ frontend/src/components/CreateEventForm/CreateEventForm.tsx
@@ -1,17 +1,37 @@
 import { Card, CardTitle, CardHeader, CardDescription, CardContent } from "../ui/card";
 import { Button } from "../ui/button";
-import { Label } from "@radix-ui/react-select";
+import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function CreateEventForm() {
+
+  const PRIVACY_LEVELS = [
+    {id: 'private', label: 'Private'},
+    {id: 'network', label: 'Network'},
+    {id: 'public', label: 'Public'},
+  ];
+  function handleSubmit(event: React.FormEvent){
+    event.preventDefault(); 
+
+
+
+
+  }
   return (
     <>
       <Card className='w-[40%] items-center justify-center p-10'>
         <CardHeader className='w-[85%]'>
-          <CardTitle> Extra Information </CardTitle>
+          <CardTitle> Add Event! </CardTitle>
           <CardDescription>
-            {" "}
-            Please fill out some extra information so we can finish setting up your profile.{" "}
+            Add your event details below! An event can be private (invite only), public (all streamers can see), or within your network (all connections can see).   
           </CardDescription>
         </CardHeader>
 
@@ -19,8 +39,13 @@ export default function CreateEventForm() {
           <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
             {/* Display Name */}
             <div className='flex flex-col gap-2'>
-              <Label htmlFor='first_name'>First Name</Label>
-              <Input id='first_name' name='first_name' placeholder='Your first name' required />
+              <Label htmlFor='event_name'>Event Name</Label>
+              <Input id='event_name' name='event_name' placeholder='Event Name' required />
+            </div>
+
+            <div className='flex flex-col gap-2'>
+              <Label htmlFor='description'>Description</Label>
+              <Textarea id='description' name='description' placeholder='Describe your event...' required />
             </div>
 
             {/* Date of birth picker */}
@@ -31,29 +56,21 @@ export default function CreateEventForm() {
 
             {/*Dropdown to select the audience that streamer streams to */}
             <div className='flex flex-col gap-2'>
-              <Label htmlFor='target_audience'>Target Audience</Label>
-              {/* <Select name='target_audience' required>
+              <Label htmlFor='privacy_level'>Privacy Level</Label>
+              <Select name='privacy_level' required>
                 <SelectTrigger className='w-full'>
-                  <SelectValue placeholder='Select your target audience' />
+                  <SelectValue placeholder='Select your events privacy level' />
                 </SelectTrigger>
                 <SelectContent>
-                  {AUDIENCES.map(audience => (
-                    <SelectItem id={audience.id} value={audience.id}>
-                      {audience.label}
+                  {PRIVACY_LEVELS.map(privacyLevel => (
+                    <SelectItem id={privacyLevel.id} value={privacyLevel.id}>
+                      {privacyLevel.label}
                     </SelectItem>
                   ))}
                 </SelectContent>
-              </Select> */}
+              </Select>
             </div>
 
-            {/*String input for tags that streamer identifies with */}
-            <div className='flex flex-col gap-2'>
-              <Label htmlFor='tags'>Content Tags</Label>
-              <Input id='tags' name='tags' placeholder='gaming, comedy, music, tutorial' required />
-              <p className='text-muted-foreground mt-1 text-sm'>
-                Separate tags with commas (e.g., gaming, comedy, music)
-              </p>
-            </div>
 
             <Button type='submit' className='bg-electric-indigo cursor-pointer'>
               Submit!
