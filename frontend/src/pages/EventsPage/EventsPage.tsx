56546e0e365e5556fe9ccb9e7c7ca54e57aabb55
Kevin Pereda <kevinpereda125@meta.com>
2025-07-15 10:00:00 -0700

    finished setting up frotnend flow so whenever page mounts events are fetched and also passe the loadEvents function to form so that on submission evemts are also refethced so user can see newly added events

--- frontend/src/pages/EventsPage/EventsPage.tsx
+++ frontend/src/pages/EventsPage/EventsPage.tsx
@@ -1,18 +1,39 @@
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CreateEventForm from "@/components/CreateEventForm/CreateEventForm";
+import { fetchEvents } from "@/lib/api_client";
+import { useAuthContext } from "@/Context/AuthProvider";
 
 export default function EventsPage() {
+  const { session } = useAuthContext();
   const [showEventForm, setShowEventForm] = useState(false);
+  const [eventList, setEventList] = useState([]);
 
-  function handleFormClose(){
+  function handleFormClose() {
     setShowEventForm(false);
   }
 
+  async function loadEvents() {
+    try {
+      if (!session?.access_token) return;
+      const events = await fetchEvents(session?.access_token);
+      setEventList(events);
+    } catch (err) {
+      console.error("could not fetch the events in the frontend", err);
+    }
+  }
+
+  useEffect(() => {
+    loadEvents();
+    console.log(eventList);
+  }, [session?.access_token]);
 
   return (
     <div className='flex flex-col items-center p-16'>
-      <Button className='bg-electric-indigo h-16 w-72 cursor-pointer' onClick={() => setShowEventForm(true)}>
+      <Button
+        className='bg-electric-indigo h-16 w-72 cursor-pointer'
+        onClick={() => setShowEventForm(true)}
+      >
         Post Event!
       </Button>
 
@@ -20,7 +41,7 @@ export default function EventsPage() {
 
       {showEventForm && (
         <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4'>
-          <CreateEventForm onClose = {handleFormClose}/>
+          <CreateEventForm onClose={handleFormClose} loadEvents={loadEvents} />
         </div>
       )}
     </div>
