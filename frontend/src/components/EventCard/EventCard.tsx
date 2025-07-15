0e2bf8053dd4aafed26462d63751687ac06f13c9
Kevin Pereda <kevinpereda125@meta.com>
2025-07-15 11:17:08 -0700

    Just applied some styles and centering so card looks a bit nicer moving on now because I have the rsvp stuff to still figure out which will be last thing and then TC2

--- frontend/src/components/EventCard/EventCard.tsx
+++ frontend/src/components/EventCard/EventCard.tsx
@@ -3,39 +3,46 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 
 export default function EventCard({ event }: any) {
+  const eventModality = event.modality === "in_person" ? "In Person" : "Online";
+
   return (
     <Card className='w-full max-w-md flex-col gap-2'>
       <CardHeader>
-        <div className='flex flex-col'>
-          <div className='flex'>
-            <h2>{event.title}</h2>
-            <Badge>{event.privacy_level}</Badge>
+        <div className='flex flex-col gap-3'>
+          <div className='flex items-center justify-between gap-4'>
+            <h2 className='text-2xl font-bold'>{event.title}</h2>
+            <Badge variant='secondary' className='text-md capitalize'>
+              {event.privacy_level}
+            </Badge>
           </div>
 
-          <div className='flex'>
-            <p>by {event.profiles.twitchUser}</p>
+          <div className='flex items-center gap-4'>
+            <p>by @ {event.profiles.twitchUser}</p>
             <img
-              className='border-light-purple h-13 w-13 rounded-full border-2 object-cover'
+              className='border-light-purple h-9 w-9 rounded-full border-2 object-cover'
               src={event.profiles.profilePic}
               alt={`${event.profiles.name}'s profile picture`}
             />
           </div>
 
-          <p className='text-sm text-gray-500'> 🗓️ {new Date(event.event_date).toLocaleDateString()} @ {new Date(event.event_date).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
+          <p className='text-sm text-gray-500'>
+            {" "}
+            🗓️ {new Date(event.event_date).toLocaleDateString()} @{" "}
+            {new Date(event.event_date).toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </p>
         </div>
       </CardHeader>
 
-      <CardContent>
-        <p className='text-sm'>{event.description}</p>
+      <CardContent className='flex flex-1 flex-col justify-center gap-4'>
+        <p className='text-sm capitalize'>{event.description}</p>
         {/*Put tags in a flex container and allow them to wrap */}
         <div className='flex flex-wrap gap-1'>
           {/*We cant display every single event in the card. Slice from the array the first 3 and map through those and display tags */}
           {event.tags.slice(0, 3).map((tag: any, index: any) => (
-            <Badge
-            variant='outline'
-            className='bg-electric-indigo text-white capitalize'
-            key={index}
-            >
+            <Badge variant='outline' className='bg-twitch-purple text-white capitalize' key={index}>
               {tag}
             </Badge>
           ))}
@@ -47,12 +54,12 @@ export default function EventCard({ event }: any) {
             </Badge>
           )}
         </div>
-        <p className='mt-2 text-xs text-gray-500'>📍 {event.modality}</p>
-        {event.location && <p className='text-xs text-gray-500'>• {event.location}</p>}
+        <p className='text-md mt-2 text-gray-500'>📍 {eventModality}</p>
+        {event.location && <p className='text-md text-gray-500 capitalize'>🗺️ {event.location}</p>}
       </CardContent>
 
-      <CardFooter>
-        <Button className='w-full'> I'm Attending</Button>
+      <CardFooter className='mt-auto'>
+        <Button className='bg-electric-indigo w-full'> I'm Attending</Button>
       </CardFooter>
     </Card>
   );
