9140198e02c2dc1da272f092c6ec642d308dd579
Kevin Pereda <kevinpereda125@meta.com>
2025-07-15 10:58:24 -0700

    just copied code from tag population from the discover streamer card only want to display 3 event tags and then show a counter for how many left

--- frontend/src/components/EventCard/EventCard.tsx
+++ frontend/src/components/EventCard/EventCard.tsx
@@ -21,12 +21,32 @@ export default function EventCard({ event }: any) {
             />
           </div>
 
-          <p className='text-sm text-gray-500'>  {new Date(event.event_date).toLocaleDateString()} @ {new Date(event.event_date).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
+          <p className='text-sm text-gray-500'> 🗓️ {new Date(event.event_date).toLocaleDateString()} @ {new Date(event.event_date).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
         </div>
       </CardHeader>
 
       <CardContent>
         <p className='text-sm'>{event.description}</p>
+        {/*Put tags in a flex container and allow them to wrap */}
+        <div className='flex flex-wrap gap-1'>
+          {/*We cant display every single event in the card. Slice from the array the first 3 and map through those and display tags */}
+          {event.tags.slice(0, 3).map((tag: any, index: any) => (
+            <Badge
+            variant='outline'
+            className='bg-electric-indigo text-white capitalize'
+            key={index}
+            >
+              {tag}
+            </Badge>
+          ))}
+
+          {/*After that check if the array length is more than the 3 we are displaying. If it is render one more badge that says how many extra tags the event has - 3 already displayed */}
+          {event.tags.length > 3 && (
+            <Badge variant='outline' className='text-xs text-gray-400'>
+              +{event.tags.length - 3}
+            </Badge>
+          )}
+        </div>
         <p className='mt-2 text-xs text-gray-500'>📍 {event.modality}</p>
       </CardContent>
 
