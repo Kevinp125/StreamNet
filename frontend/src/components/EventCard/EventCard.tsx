b53038b75e72c3f7a2b818b0a75364a489ca68b8
Kevin Pereda <kevinpereda125@meta.com>
2025-07-15 10:47:44 -0700

    formatting time displayed on the card

--- frontend/src/components/EventCard/EventCard.tsx
+++ frontend/src/components/EventCard/EventCard.tsx
@@ -10,22 +10,24 @@ export default function EventCard({ event }: any) {
           <div className='flex'>
             <h2>{event.title}</h2>
             <Badge>{event.privacy_level}</Badge>
-
           </div>
 
           <div className='flex'>
-
-
-
+            <p>by {event.profiles.twitchUser}</p>
+            <img
+              className='border-light-purple h-13 w-13 rounded-full border-2 object-cover'
+              src={event.profiles.profilePic}
+              alt={`${event.profiles.name}'s profile picture`}
+            />
           </div>
 
-
+          <p className='text-sm text-gray-500'>  {new Date(event.event_date).toLocaleDateString()} @ {new Date(event.event_date).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
         </div>
       </CardHeader>
 
       <CardContent>
         <p className='text-sm'>{event.description}</p>
-        <p className='text-xs text-gray-500 mt-2'>📍 {event.modality}</p>
+        <p className='mt-2 text-xs text-gray-500'>📍 {event.modality}</p>
       </CardContent>
 
       <CardFooter>
