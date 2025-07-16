d3872e0375a0395247b67362bd678b1ec7ebb7d4
Kevin Pereda <kevinpereda125@meta.com>
2025-07-16 12:38:21 -0700

    Getting basic layout for component. This component is where all notifications important and general ones will be displayed

---
+++ frontend/src/components/NotificationList/NotificationList.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Check, X } from "lucide-react";
+import { useAuthContext } from "@/Context/AuthProvider";
+import { setConnectionRequestStatusAndPostIfAccept } from "@/lib/api_client";
+
+
+export default function NotificationList(){
+
+
+
+  
+}
