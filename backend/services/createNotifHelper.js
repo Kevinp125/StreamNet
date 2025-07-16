21caeee20dd05ed2d354c025b95ee42dc730d8a5
Kevin Pereda <kevinpereda125@meta.com>
2025-07-16 11:43:07 -0700

    Created a helper function in services that will add notifications to my table depending on event that is triggered. Takes in parameters to customize notification types and messages depending on event that occurs

---
+++ backend/services/createNotifHelper.js
@@ -0,0 +1,22 @@
+//This function is a helper that allows us to make a notif in table after certain triggers
+
+async function createNotification(
+  supabaseClient,
+  { userId, type, title, message, data = {}, priority = "general" }
+) {
+  try {
+    const { error } = await supabaseClient.from("notifications").insert({
+      user_id: userId,
+      type,
+      title,
+      message,
+      data,
+      priority,
+    });
+
+    if (error) throw error;
+  } catch (err) {
+    console.error("Could not create a notification", err);
+    throw error;
+  }
+}
