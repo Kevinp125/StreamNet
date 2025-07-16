bb84e88d9ded1bfb6cbfc2a15fdc2d16d0b02d83
Kevin Pereda <kevinpereda125@meta.com>
2025-07-16 12:44:42 -0700

    Getting typing of notifiaiotn and setting up state that will display notifications

--- frontend/src/components/NotificationList/NotificationList.tsx
+++ frontend/src/components/NotificationList/NotificationList.tsx
@@ -4,10 +4,18 @@ import { Check, X } from "lucide-react";
 import { useAuthContext } from "@/Context/AuthProvider";
 import { setConnectionRequestStatusAndPostIfAccept } from "@/lib/api_client";
 
+type Notification = {
+  id: string;
+  type: string;
+  title: string;
+  message: string;
+  data: any;
+  status: string;
+  priority: string;
+  created_at: string;
+};
 
-export default function NotificationList(){
-
-
-
-  
+export default function NotificationList() {
+  const { session } = useAuthContext();
+  const [notifications, setNotifications] = useState<Notification[]>([]);
 }
