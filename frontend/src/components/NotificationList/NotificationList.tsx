45218ef9e9d45bfc4030a43a3891217d56d07d97
Kevin Pereda <kevinpereda125@meta.com>
2025-07-16 12:51:20 -0700

    added logic so that on component mount the notiifactions are fetched:

--- frontend/src/components/NotificationList/NotificationList.tsx
+++ frontend/src/components/NotificationList/NotificationList.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Check, X } from "lucide-react";
 import { useAuthContext } from "@/Context/AuthProvider";
+import { fetchNotifications } from "@/lib/api_client";
 import { setConnectionRequestStatusAndPostIfAccept } from "@/lib/api_client";
 
 type Notification = {
@@ -20,20 +21,19 @@ export default function NotificationList() {
   const [notifications, setNotifications] = useState<Notification[]>([]);
 
   useEffect(() => {
-    async function getNotifications(){
-      if(!session?.access_token) return;
-
-
-
-
-
+    async function getNotifications() {
+      if (!session?.access_token) return;
+      try {
+        const notifications = await fetchNotifications(session.access_token);
+        setNotifications(notifications);
+      } catch (err) {
+        console.error("Failed to get notifs", err);
+      }
     }
 
     getNotifications();
+  }, [session?.access_token]);
 
 
-
-
-
-  }, [session?.access_token])
+  
 }
