f563b7266dff5711fbb41b1cf59953ebcb40abba
Kevin Pereda <kevinpereda125@meta.com>
2025-07-17 08:08:10 -0700

    Setting up initial code to get a websocket going

---
+++ backend/websocket/websocketServer.js
@@ -0,0 +1,11 @@
+const WebSocket = require('ws');
+
+//need to make a websocket server that listens on port 8080 (standard)
+
+const wss = new WebSocket.Server({port:8080});
+
+//this map is going allow us to store each user when they come online along with their ws object (connection)
+// clients = userId: WebSocket connection
+const clients = new Map();
+
+//everything for websockets is event driven...
