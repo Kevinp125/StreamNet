ad1d8bebe2346039657f195176f80f0177753870
Kevin Pereda <kevinpereda125@meta.com>
2025-07-17 09:12:10 -0700

    if the user has a session once ws connection is made (onopen) we send server message so we can get put in map of active connections

--- frontend/src/Context/WebSocketProvider.tsx
+++ frontend/src/Context/WebSocketProvider.tsx
@@ -4,15 +4,40 @@ import { useAuthContext } from "./AuthProvider";
 type WebSocketContextType = {
   socket: WebSocket | null; //can be of type websocket or null ehrn there isnt a connection yet
   isConnected: boolean;
-}
+};
 
 const WebSocketContext = createContext<WebSocketContextType>({
-  socket:null,
-  isConnected:false
+  socket: null,
+  isConnected: false,
 });
 
-export function WebSocketProvider({children}: {children: ReactNode}){
+export function WebSocketProvider({ children }: { children: ReactNode }) {
   const [socket, setSocket] = useState<WebSocket | null>(null);
   const [isConnected, setIsConnected] = useState(false);
-  const { session } = useAuthContext();
+  const { session } = useAuthContext(); //we dont want to establish socket connection if user isnt authenticated / logged in
+
+  useEffect(() => {
+    if (session?.user?.id) {
+      //if we have a session currently we want to establish a connection...
+      console.log("Making WebSocket connection..");
+
+      //starting connection with server
+      const ws = new WebSocket("ws://localhost:8080");
+
+      //we do onopen with a callback because it checks if websocket connection was processed by server first
+      ws.onopen = () => {
+        console.log("WebSocket Connected");
+        setIsConnected(true); //update that we are connected
+
+
+        //send the server that we want to officially connect. Server processes this message and stores us in map of connections
+        ws.send(
+          JSON.stringify({
+            type: "auth",
+            userId: session.user.id,
+          }),
+        );
+      };
+    }
+  }, [session?.user?.id]);
 }
