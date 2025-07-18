66c386b317b75c971b81bbad4b1b5fe57d07bce0
Kevin Pereda <kevinpereda125@meta.com>
2025-07-18 10:37:03 -0700

    didnt like purple look went with white background and it will have the purple for the toggles and save button

--- frontend/src/components/NotificationSettingsModal/NotifcationSettingsModal.tsx
+++ frontend/src/components/NotificationSettingsModal/NotifcationSettingsModal.tsx
@@ -7,15 +7,24 @@ type NotificationSettingsModalProps = {
 
 export default function NotificationSettingsModal({ onClose }: NotificationSettingsModalProps) {
   return (
-    <section className='bg-twitch-purple max-h-[80vh] w-full max-w-xl overflow-hidden rounded-lg shadow-xl'>
+    <section className='max-h-[80vh] w-full max-w-xl overflow-hidden rounded-lg bg-white shadow-xl'>
       <header className='flex items-center justify-between border-b border-gray-200 p-6'>
-        <h3 className='text-xl font-semibold text-white'>Notification Settings 🔔</h3>
-        <X onClick = {onClose} className='h-6 w-6 text-white cursor-pointer transition-transform duration-200 hover:text-red-500' />
+        <h3 className='text-xl font-semibold'>Notification Settings 🔔</h3>
+        <X
+          onClick={onClose}
+          className='h-6 w-6 cursor-pointer transition-transform duration-200 hover:text-red-500'
+        />
       </header>
 
-      <article></article>
+      <article className='p-6'>
+        <p>Settings will go here...</p>
+      </article>
 
-      <footer></footer>
+      <footer className='flex justify-center border-t border-white bg-gray-100 px-6 py-4'>
+        <button className='bg-twitch-purple w-3/4 cursor-pointer rounded-lg px-4 py-2 font-medium text-white'>
+          Save Settings
+        </button>
+      </footer>
     </section>
   );
 }
