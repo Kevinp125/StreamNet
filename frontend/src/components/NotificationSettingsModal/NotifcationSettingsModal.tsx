d4fcd3b6b66d5f3f0eca166a8e2b711b2d2181bf
Kevin Pereda <kevinpereda125@meta.com>
2025-07-18 10:30:35 -0700

    getting basic shape of modal going. Header section is filled out with the title of the popup as well as the close button adn bar below it to separate it from the options

--- frontend/src/components/NotificationSettingsModal/NotifcationSettingsModal.tsx
+++ frontend/src/components/NotificationSettingsModal/NotifcationSettingsModal.tsx
@@ -1,19 +1,21 @@
 import { X } from "lucide-react";
-
+import DiscoverPage from "../../pages/DiscoverPage/DiscoverPage";
 
 type NotificationSettingsModalProps = {
   onClose: () => void;
+};
 
-}
+export default function NotificationSettingsModal({ onClose }: NotificationSettingsModalProps) {
+  return (
+    <section className='bg-twitch-purple max-h-[80vh] w-full max-w-xl overflow-hidden rounded-lg shadow-xl'>
+      <header className='flex items-center justify-between border-b border-gray-200 p-6'>
+        <h3 className='text-xl font-semibold text-white'>Notification Settings 🔔</h3>
+        <X onClick = {onClose} className='h-6 w-6 text-white cursor-pointer transition-transform duration-200 hover:text-red-500' />
+      </header>
 
-export default function NotificationSettingsModal({onClose}: NotificationSettingsModalProps){
-  return(
-    <div className = "bg-electric-indigo">
-      <X onClick = {onClose}/>
-    </div>
-    
-  )
+      <article></article>
 
-
-
+      <footer></footer>
+    </section>
+  );
 }
