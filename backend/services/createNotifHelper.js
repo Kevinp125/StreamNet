9626dc77ac61c4de5cce732083680ff6dc14c589
Kevin Pereda <kevinpereda125@meta.com>
2025-07-22 09:06:19 -0700

    added comment to websocket file at the top reminding me to remove console logs once done those cant be in prod. Also adding logic to mycreate notification function. Whenever a notification is made we need to check the users active window. If the current hour is within that window we set status of noitfication to delivered otherwise swap it to pending

--- backend/services/createNotifHelper.js
+++ backend/services/createNotifHelper.js
@@ -1,4 +1,5 @@
 const { sendNotificationToUser } = require("../websocket/websocketServer");
+const { getUsersActiveWindow } = require("./activityAnalysis");
 
 //This function is a helper that allows us to make a notif in table after certain triggers
 //we also call the sendNotificationToUser function that attempts to send same notification
@@ -8,6 +9,19 @@ async function createNotification(
   { userId, type, title, message, contextData = {}, priority = "general" }
 ) {
   try {
+    //lets let the status of a notif be delivered by default
+    let status = "delivered";
+
+    if (priority === "general") {
+      const window = await getUsersActiveWindow(userId);
+      const currentHour = new Date().getHours();
+
+      if (currentHour >= window.start && currentHour < window.end) {
+        status = "delivered"; // Send now - user will see it
+      } else {
+        status = "pending"; // Queue for later - user won't see it yet
+      }
+    }
     //gotta get notification back when its created so we can try to notify user if they are online
     const { data: notification, error } = await supabaseClient
       .from("notifications")
@@ -33,7 +47,6 @@ async function createNotification(
         `User ${userId} offline, notification stored in database only`
       );
     }
-    
   } catch (err) {
     console.error("Could not create a notification", err);
     throw err;
