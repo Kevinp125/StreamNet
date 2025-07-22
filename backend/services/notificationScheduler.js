0200725e4790f075aa23da69dd17ca657ad7a291
Kevin Pereda <kevinpereda125@meta.com>
2025-07-22 10:21:23 -0700

    starting to add logic for background job. First we ned to get all the users with pending notifications so we can check which ones fall withih active window and update

--- backend/services/notificationScheduler.js
+++ backend/services/notificationScheduler.js
@@ -4,7 +4,28 @@ const { sendNotificationToUser } = require("../websocket/websocketServer");
 //function will check what users fall within
 //active window during a set interval and call a helper function
 //that updates each users notifications
-async function deliverPendingNotifications() {}
+async function deliverPendingNotifications() {
+  try {
+    const currentHour = new Date().getHours();
+
+    //we need to get all users who have pending notifs
+    //keep in mind this might return duplicate userIds since one user
+    //might have multiple pending notifications
+    const { data: usersWithPending, error: usersError } = await supabase
+      .from("notifications")
+      .select("user_id")
+      .eq("status", "pending")
+      .neq("user_id", null);
+
+    if (usersError) {
+      console.error(
+        "Error fetching users with pending notifications:",
+        usersError
+      );
+      return;
+    }
+  } catch (err) {}
+}
 
 //function will change noitfication status from pending to delievered
 //to individual users
