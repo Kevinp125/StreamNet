b7938957718ddd5d5753868b5128eaddd8a8174b
Kevin Pereda <kevinpereda125@meta.com>
2025-07-23 10:01:05 -0700

    starting to add logic to find notifications that need a nudge. We qeury notifications and grab the ones that still have a status of seen are the type of actionable ones we want and are older than 3 hours (user saw them but did nothing for 3 hours

--- backend/services/nudgeScheduler.js
+++ backend/services/nudgeScheduler.js
@@ -2,6 +2,22 @@ const { supabase } = require("./supabaseclient");
 const { getUsersActiveWindow } = require("./activityAnalysis");
 
 async function findNotificationsNeedingNudge(){
+  try{
+    //subtracing 3 hours from current time
+    const threeHoursAgo = new Date(Date.now()- 3 * 60 * 60 *1000).toISOString(); 
 
-  
+    const {data: notifications, error} = await supabase
+      .from("notifications")
+      .select("*")
+      .eq("status", "seen")
+      .in("type", ["connection_request", "private_event_invitation"])
+      .lt("seen_at", threeHoursAgo);
+    
+    if(error) throw error;
+
+  } catch (err){
+    console.error("Error trying to find notifications that need a nudge", err);
+    return [];
+  }
+
 }
