01d54041dcad34a95114da683cd34dbb74f14824
Kevin Pereda <kevinpereda125@meta.com>
2025-07-24 15:01:38 -0700

    Another unit test passing that checks calc age score for whenever users are within 5 years not 2

--- backend/tests/calculateMatchScore.test.js
+++ backend/tests/calculateMatchScore.test.js
@@ -8,4 +8,11 @@ describe('calcAgeScore', () => {
     
     expect(calcAgeScore(user1, user2)).toBe(2)
   })
+
+  it('should return 1 when ages are within 5 years (close)', () => {
+    const user1 = { date_of_birth: '2000-01-01' }
+    const user2 = { date_of_birth: '2004-01-01' } // 4 year difference
+    
+    expect(calcAgeScore(user1, user2)).toBe(1)
+  })
 })
