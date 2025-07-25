5aa3d99bc31df7516e26605a5d262f32b00c2c80
Kevin Pereda <kevinpereda125@meta.com>
2025-07-25 09:54:06 -0700

    making merge and dupe array function a little more safer. In case tag array is empty for whatever reason shoulndt be

--- backend/services/mergeArrays.js
+++ backend/services/mergeArrays.js
@@ -1,5 +1,8 @@
 function mergeAndDeduplicateTags(tags1 = [], tags2 =[]) {
-  const allTags = [...tags1, ...tags2];
+  const safeTag1 = Array.isArray(tags1) ? tags1 : [];
+  const safeTag2 = Array.isArray(tags2) ? tags2 : [];
+  
+  const allTags = [...safeTag1, ...safeTag2];
   return [...new Set(allTags.map(tag => tag.toLowerCase()))];
 }
 
