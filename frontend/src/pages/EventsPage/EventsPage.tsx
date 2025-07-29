31d12ba23c28b3119d4911c77f3c52c937fcf798
Kevin Pereda <kevinpereda125@meta.com>
2025-07-29 10:55:17 -0700

    just added a loadign state to events page as well and if there are no events we display no event smessage and keep the button and form up so user can add event

--- frontend/src/pages/EventsPage/EventsPage.tsx
+++ frontend/src/pages/EventsPage/EventsPage.tsx
@@ -5,11 +5,13 @@ import { fetchEvents } from "@/lib/api_client";
 import { postEventRsvp } from "@/lib/api_client";
 import { useAuthContext } from "@/Context/AuthProvider";
 import EventsGrid from "@/components/EventsGrid /EventsGrid";
+import { Loader2 } from "lucide-react";
 
 export default function EventsPage() {
   const { session } = useAuthContext();
   const [showEventForm, setShowEventForm] = useState(false);
   const [eventList, setEventList] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   function handleFormClose() {
     setShowEventForm(false);
@@ -38,6 +40,7 @@ export default function EventsPage() {
       if (!session?.access_token) return;
       const events = await fetchEvents(session?.access_token);
       setEventList(events);
+      setLoading(false);
     } catch (err) {
       console.error("could not fetch the events in the frontend", err);
     }
@@ -46,6 +49,36 @@ export default function EventsPage() {
   useEffect(() => {
     loadEvents();
   }, [session?.access_token]);
+
+  if (loading) {
+    return (
+      <div className='flex min-h-screen items-center justify-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-white' />
+        <div className='font-bold text-white'>Finding events...</div>
+      </div>
+    );
+  }
+
+  if (eventList.length === 0) {
+    return (
+      <div className='flex flex-col items-center gap-60 p-16'>
+        <Button
+          className='bg-electric-indigo h-16 w-72 cursor-pointer hover:bg-purple-700'
+          onClick={() => setShowEventForm(true)}
+        >
+          Post Event!
+        </Button>
+
+        <div className='mb-2 text-xl font-bold text-white'>No events yet</div>
+
+        {showEventForm && (
+          <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4'>
+            <CreateEventForm onClose={handleFormClose} loadEvents={loadEvents} />
+          </div>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div className='flex flex-col items-center p-16'>
